f71553dc1c0ba84f6f9e64702118ea420e4820b4
Daniela Mota <danielafcmota.2006@gmail.com>
2024-03-23 13:57:42 +0000

    condições

---
+++ exercicios/aula8/ex008.js
@@ -0,0 +1,9 @@
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60){
+    console.log("Ultrapassou a velocidade permitida. Multado!")
+} 
+console.log("Dirija sempre usando cinto de segurança.")
+
+
+
