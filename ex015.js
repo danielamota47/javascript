6102509428cd368eda40b08ce9386cced029e191
Daniela Mota <danielafcmota.2006@gmail.com>
2024-03-25 21:10:58 +0000

    repetições

---
+++ ex015.js
@@ -0,0 +1,15 @@
+
+// testa + executa
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+} 
+
+//executa + testa
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
