b317499a595da45ef4f29d58247b6d6ba3b1a133
Daniela Mota <danielafcmota.2006@gmail.com>
2024-03-27 09:03:34 +0000

    d001

---
+++ outrosdesafios/d001/script.js
@@ -0,0 +1,15 @@
+var b1 = window.document.getElementById('b1')
+var b2 = window.document.querySelector('button#b2')
+var b3 = window.document.getElementById('b3')
+
+function click1() {
+    window.alert("Você carregou no botão 1")
+}
+
+function click2() {
+    window.alert("Você carregou no botão 2")
+}
+
+function click3() {
+    window.alert("Você carregou no botão 3")
+}
