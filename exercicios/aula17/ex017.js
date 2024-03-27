a9466d1479da6e5acc268f2a8bcfbf563d5f2df8
Daniela Mota <danielafcmota.2006@gmail.com>
2024-03-27 19:56:37 +0000

    ex017

---
+++ exercicios/aula17/ex017.js
@@ -0,0 +1,13 @@
+let num = [2,3,1]
+console.log(num)
+
+console.log(`O nosso array tem ${num.length} elementos`)
+
+console.log(`O primeiro valor do vetor é ${num[0]}.`)
+
+console.log(`Por ordem crescente, os nossos elementos são: ${num.sort()}.`)
+
+num[3] = 4
+num.push(5)
+
+console.log(`Agora o nosso vetor é ${num} e possui ${num.length} elementos.`)
