79c082360150e14db719dff96194a125599bb80d
Daniela Mota <danielafcmota.2006@gmail.com>
2024-03-27 11:00:24 +0000

    for

---
+++ exercicios/aula16/ex016.js
@@ -0,0 +1,21 @@
+
+for (c=1; c<=5; c++) {
+    console.log(`Passo ${c}`)
+}
+
+/*
+igual a:
+
+var c= 1
+while (c<=5 ) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+console.log("Vai começar...")
+for(var v = 1; v <= 4; v++) {
+    console.log(v)
+}
+console.log("FIM")
+
